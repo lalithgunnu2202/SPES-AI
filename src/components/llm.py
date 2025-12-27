d213617eea7573c38d54c6b6d4498f9c13dc1a4d
Lalith <lalithgunnu2202@gmail.com>
2025-12-27 20:27:03 +0530

    SPES AI v1.0 done

---
+++ src/components/llm.py
@@ -0,0 +1,42 @@
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+import json
+import pprint
+from src.components.online import run
+from src.components.main import send_text
+
+class OpenrouterClient:
+    def __init__(self,api_key, base_url="https://openrouter.ai/api/v1"):
+        self.api_key = api_key
+        self.base_url = base_url
+        # self.header={
+        #     "Authorization":f"bearer {api_key}",
+        #     "content-type":"application/json"
+        # }
+
+    def chat_completion(self,model="openai/gpt-oss-20b:free", messages=None,temperature=0.7):
+        client = OpenAI(
+            base_url=self.base_url,
+            api_key=self.api_key
+        )
+        response = client.chat.completions.create(
+            model=model,
+            messages=[{
+                "role":"user",
+                "content":f"Answer this in under 50 words {messages}",
+            }],
+            temperature=temperature
+        )
+        matter = response.choices[0].message.content
+        return matter
+
+def prepare_message(query):
+    load_dotenv()
+    send_text(query)
+    data = run()
+    custom_api_key = os.getenv("CUSTOM_API_KEY")
+    client = OpenrouterClient(custom_api_key)
+    response = client.chat_completion(messages=f"{data} write this data in under 50 words")
+    print(response)
+    return response
